30bfe36912a3e8e6ade6e5ae73a9edea0a3c6af0
rostislav <rastowerin@gmail.com>
2019-07-22 21:34:43 +0300

    валентность

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import parsing
 
 
-input = [list('2H2So4'), 1]
+input = '2H6(PO4)2'
 
 
 def recognition(input):
+    input = list(input)
+    if input[0].isdigit():
+        input = [input[1:], int(input[0])]
+    else:
+        input = [input, 1]
     for symbol in range(len(input[0])):
         try:
             if input[0][symbol].islower():
@@ -35,6 +40,7 @@ def recognition(input):
             input[0][input[0].index(symbol)] = [symbol, '1']
     return input
 
+
 def definition(input):
     for symbol in input[0]:
         if symbol[0] in parsing.all_elements:
@@ -42,9 +48,18 @@ def definition(input):
                 input[0][input[0].index(symbol)].append('non_metal')
             else:
                 input[0][input[0].index(symbol)].append('metal')
+            for x in parsing.valence.keys():
+                if symbol[0] == x and len(parsing.valence[x]) == 1:
+                        input[0][input[0].index(symbol)].append(parsing.valence[symbol[0]][0])
+
     if len(input[0]) == 2 and input[0][1][0] == 'O':
         input.append(['oxide'])
-    elif input[0][0][2] == 'metal':
+    else:
+        input[0] = [input[0][0], input[0][1:]]
+        input[0][1] = [input[0][1]]
+        input[0][1] = [input[0][1][0], acid_residue_valence(input)]
+
+    if input[0][0][2] == 'metal':
         result = ''
         for x in input[0][1:]:
             result += x[0]
@@ -58,17 +73,48 @@ def definition(input):
                     if z[0] == result:
                         input.append('salt')
     elif input[0][0][0] == 'H':
-        result = ''
-        for a in input[0][1:]:
-            result += a[0]
-            if a[1] != '1':
-                result += a[1]
-                print(result)
-        for b in parsing.table:
-            for c in b:
-                if c[0] == result:
-                    input.append('acid')
+        acid_residue(input)
+        input.append('acid')
+
+
+    determinded_valence = 0
+    for x in input[0][1][0]:
+        if len(x) == 4:
+            determinded_valence += int(x[1]) * x[3]
+
+    for y in input[0][1][0]:
+        if len(y) == 3:
+            input[0][1][0][input[0][1][0].index(y)].append(determinded_valence // int(y[1]) - int(input[0][1][1]))
+            if input[0][1][0][input[0][1][0].index(y)][3] not in parsing.valence[input[0][1][0][input[0][1][0].index(y)][0]]:
+                raise KeyError
+
+    print(input[0][0])
+#    if len(input[0][0]) == 3:
+#        input[0][0].append(int(input[0][1][3])*)
+
+#        print(input)
+#    if input[0][0][1]*input[0][0][3] != input[0][1][1]*input[0][1][3]:
+#        raise TypeError
     print(input)
+
+
+def acid_residue_valence(input):
+    for b in parsing.table:
+        for c in b:
+            if c[0] == acid_residue(input):
+                return c[1][0]
+    raise ValueError
+
+def acid_residue(input):
+    result = ''
+    for a in input[0][1][0]:
+        if input[0][1][0].index(a) != 0:
+            result += a[0].lower()
+        else:
+            result += a[0][0]
+        if a[1] != '1':
+            result += a[1][0]
+    return result
 
 
 definition(recognition(input))
--- parsing.py
+++ parsing.py
@@ -1,6 +1,7 @@
 import urllib.request
 from bs4 import BeautifulSoup
 
+
 r = urllib.request.urlopen('http://www.sev-chem.narod.ru/spravochnik/rastvor.htm')
 soup = BeautifulSoup(r.read(), features = "lxml")
 
@@ -29,21 +30,77 @@ for x in result:
                             element[0] = split1 + element[0][1:][element[0][1:].index(z)].lower() + split2
         table[result.index(x)].append(element)
 
-
 r = urllib.request.urlopen('https://ru.wikipedia.org/wiki/%D0%9D%D0%B5%D0%BC%D0%B5%D1%82%D0%B0%D0%BB%D0%BB%D1%8B')
-soup = BeautifulSoup(r.read(), features = "lxml")
+soup = BeautifulSoup(r.read(), features="lxml")
 
 result = soup.find('tbody').find_all('a')
 non_metals = []
 
 for x in result:
     non_metals.append(x.text)
+
+
 r = urllib.request.urlopen('https://ru.wikipedia.org/wiki/%D0%A1%D0%BF%D0%B8%D1%81%D0%BE%D0%BA_%D1%85%D0%B8%D0%BC%D0%B8%D1%87%D0%B5%D1%81%D0%BA%D0%B8%D1%85_%D1%8D%D0%BB%D0%B5%D0%BC%D0%B5%D0%BD%D1%82%D0%BE%D0%B2')
-soup = BeautifulSoup(r.read(), features = "lxml")
+soup = BeautifulSoup(r.read(), features="lxml")
 all_elements = []
 
-result = soup.find_all('table')[2].find_all('tr')
+result = soup.find_all('table')[1].find_all('tr')
 
 for x in result:
     if result.index(x) != 0:
         all_elements.append(x.find_all('td')[2].text)
+
+
+r = urllib.request.urlopen('https://ru.wikipedia.org/wiki/%D0%A1%D0%BF%D0%B8%D1%81%D0%BE%D0%BA_%D1%81%D1%82%D0%B5%D0%BF%D0%B5%D0%BD%D0%B5%D0%B9_%D0%BE%D0%BA%D0%B8%D1%81%D0%BB%D0%B5%D0%BD%D0%B8%D1%8F_%D1%8D%D0%BB%D0%B5%D0%BC%D0%B5%D0%BD%D1%82%D0%BE%D0%B2')
+soup = BeautifulSoup(r.read(), features="lxml")
+
+result = soup.find('tbody').find_all('tr')
+
+result1 = []
+result2 = []
+valence = {}
+
+for x in result:
+    for y in x.find_all('td'):
+        result1.append(y.text)
+
+for x in result1:
+    if x.isalpha():
+        result2.append(x)
+    for y in ['+', '-', '−']:
+        if y in x:
+            result2.append(x)
+
+for x in result2:
+    element_valence = []
+    if x.isalpha():
+        y = 1
+        while not result2[result2.index(x) - y].isalpha():
+            if result2[result2.index(x) - y][0] in ['-', '−']:
+                if int(result2[result2.index(x) - y][1]) not in element_valence:
+                    element_valence.append(int(result2[result2.index(x) - y][1]))
+            y += 1
+
+        try:
+            y = 1
+            while not result2[result2.index(x) + y].isalpha():
+                if result2[result2.index(x) + y][0] == '+':
+                    if int(result2[result2.index(x) + y][1]) not in element_valence:
+                        element_valence.append(int(result2[result2.index(x) + y][1]))
+                y += 1
+        except IndexError:
+            None
+
+        valence[x] = element_valence
+
+def restrictions(valence):
+    for x in valence.keys():
+        if x == 'O':
+            valence[x] = [2]
+        elif x == 'Fe':
+            valence[x] = [2, 3]
+
+restrictions(valence)
+
+
+#print(valence)
